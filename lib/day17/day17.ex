1fb2c9e3505461cd04688154ff87d9885aba05a0
jkmrto <juanka1331@gmail.com>
2020-12-20 00:18:46 +0100

    day17

--- lib/day17/day17.ex
+++ lib/day17/day17.ex
@@ -1,8 +1,16 @@
 defmodule Aoc2020.Day17 do
-  @neigborhods for(n <- [-1, 0, 1], m <- [-1, 0, 1], k <- [-1, 0, 1], do: {m, n, k}) --
-                 [{0, 0, 0}]
+  @neigborhods_3 for(n <- [-1, 0, 1], m <- [-1, 0, 1], k <- [-1, 0, 1], do: {m, n, k}) --
+                   [{0, 0, 0}]
+  @neigborhods_4 for(
+                   n <- [-1, 0, 1],
+                   m <- [-1, 0, 1],
+                   k <- [-1, 0, 1],
+                   l <- [-1, 0, 1],
+                   do: {m, n, k, l}
+                 ) --
+                   [{0, 0, 0, 0}]
 
-  def read(file) do
+  def read(file, mode) do
     file
     |> File.stream!([], :line)
     |> Enum.map(&String.trim/1)
@@ -14,46 +22,73 @@ defmodule Aoc2020.Day17 do
       new_actives =
         row
         |> Enum.filter(&(elem(&1, 0) == "#"))
-        |> Enum.map(fn {_, x} -> {x, y, 0} end)
+        |> Enum.map(fn {_, x} ->
+          case mode do
+            :dim_3 -> {x, y, 0}
+            :dim_4 -> {x, y, 0, 0}
+          end
+        end)
 
       new_actives ++ actives
     end)
   end
 
   def task1(file) do
-    actives = read(file)
+    actives = read(file, :dim_3)
     run(actives, 1)
   end
 
-  def count_actives({x, y, z}, actives) do
-    Enum.reduce(@neigborhods, 0, fn {x_n, y_n, z_n}, acc ->
-      if {x + x_n, y + y_n, z + z_n} in actives, do: acc + 1, else: acc
-    end)
+  def task2(file) do
+    actives = read(file, :dim_4)
+    run(actives, 1)
   end
 
   def run(actives, iter) when iter > 6, do: length(actives)
 
   def run(actives, iter) do
+    IO.puts("Iter: #{iter}")
+
     keep_actives =
       Enum.reduce(actives, [], fn cell, acc ->
         if count_actives(cell, actives) in [2, 3], do: [cell | acc], else: acc
       end)
 
-    actives_neigborhods =
-      Enum.reduce(actives, [], fn {x, y, z}, acc ->
-        Enum.reduce(@neigborhods, acc, fn
-          {x_n, y_n, z_n}, acc -> [{x + x_n, y + y_n, z + z_n} | acc]
-        end)
-      end)
-
-    actives_neigborhods = actives_neigborhods |> Enum.uniq()
-
     new_actives =
-      Enum.reduce(actives_neigborhods, [], fn cell, acc ->
+      actives
+      |> get_inactives_neighbours()
+      |> Enum.uniq()
+      |> Enum.reduce([], fn cell, acc ->
         if count_actives(cell, actives) == 3, do: [cell | acc], else: acc
       end)
 
-    all_actives = Enum.uniq(keep_actives ++ new_actives)
-    run(all_actives, iter + 1)
+    run(Enum.uniq(keep_actives ++ new_actives), iter + 1)
+  end
+
+  def count_actives({x, y, z}, actives) do
+    Enum.reduce(@neigborhods_3, 0, fn {x_n, y_n, z_n}, acc ->
+      if {x + x_n, y + y_n, z + z_n} in actives, do: acc + 1, else: acc
+    end)
+  end
+
+  def count_actives({x, y, z, w}, actives) do
+    Enum.reduce(@neigborhods_4, 0, fn {x_n, y_n, z_n, w_n}, acc ->
+      if {x + x_n, y + y_n, z + z_n, w + w_n} in actives, do: acc + 1, else: acc
+    end)
+  end
+
+  def get_inactives_neighbours(actives = [{_, _, _} | _]) do
+    Enum.reduce(actives, [], fn {x, y, z}, acc ->
+      Enum.reduce(@neigborhods_3, acc, fn
+        {x_n, y_n, z_n}, acc -> [{x + x_n, y + y_n, z + z_n} | acc]
+      end)
+    end)
+  end
+
+  def get_inactives_neighbours(actives = [{_, _, _, _} | _]) do
+    Enum.reduce(actives, [], fn {x, y, z, w}, acc ->
+      Enum.reduce(@neigborhods_4, acc, fn
+        {x_n, y_n, z_n, w_n}, acc -> [{x + x_n, y + y_n, z + z_n, w + w_n} | acc]
+      end)
+    end)
   end
 end
